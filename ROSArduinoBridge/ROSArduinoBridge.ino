7a6528da4f150c16b8160ec26dccf8d23747960c
Lolo <loicfinette@gmail.com>
2025-06-06 20:35:33 +0200

    addded code

--- ROSArduinoBridge/ROSArduinoBridge.ino
+++ ROSArduinoBridge/ROSArduinoBridge.ino
@@ -1,153 +1,92 @@
+#include <Servo.h>
+
 /*********************************************************************
  *  ROSArduinoBridge
- 
-    A set of simple serial commands to control a differential drive
-    robot and receive back sensor and odometry data. Default 
-    configuration assumes use of an Arduino Mega + Pololu motor
-    controller shield + Robogaia Mega Encoder shield.  Edit the
-    readEncoder() and setMotorSpeed() wrapper functions if using 
-    different motor controller or encoder method.
-
-    Created for the Pi Robot Project: http://www.pirobot.org
-    and the Home Brew Robotics Club (HBRC): http://hbrobotics.org
-    
-    Authors: Patrick Goebel, James Nugen
-
-    Inspired and modeled after the ArbotiX driver by Michael Ferguson
-    
-    Software License Agreement (BSD License)
-
-    Copyright (c) 2012, Patrick Goebel.
-    All rights reserved.
-
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions
-    are met:
-
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above
-       copyright notice, this list of conditions and the following
-       disclaimer in the documentation and/or other materials provided
-       with the distribution.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
+ *  ...
  *********************************************************************/
 
 #define USE_BASE      // Enable the base controller code
-//#undef USE_BASE     // Disable the base controller code
 
-/* Define the motor controller and encoder library you are using */
 #ifdef USE_BASE
-   /* The Pololu VNH5019 dual motor driver shield */
-   //#define POLOLU_VNH5019
-
-   /* The Pololu MC33926 dual motor driver shield */
-   //#define POLOLU_MC33926
-
-   /* The RoboGaia encoder shield */
-   //#define ROBOGAIA
-   
-   /* Encoders directly attached to Arduino board */
    #define ARDUINO_ENC_COUNTER
-
-   /* L298 Motor driver*/
-   //#define L298_MOTOR_DRIVER
-
    #define ESCON_MAXON
    #define GRABBER
    #define BATTERY
    #define CUSTOM_SERVOS
 #endif
 
-//#define USE_SERVOS  // Enable use of PWM servos as defined in servos.h
 #undef USE_SERVOS     // Disable use of PWM servos
 
-/* Serial port baud rate */
-#define BAUDRATE     57600
-
-/* Maximum PWM signal */
+#define BAUDRATE       57600
 #define MAX_PWM        255
 
 #if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
+  #include "Arduino.h"
 #else
-#include "WProgram.h"
+  #include "WProgram.h"
 #endif
 
-/* Include definition of serial commands */
 #include "commands.h"
-
-/* Sensor functions */
 #include "sensors.h"
-
 #include "sensors_updated.h"
 
-/* Include servo support if required */
 #ifdef USE_SERVOS
-   #include <Servo.h>
-   #include "servos.h"
+  #include <Servo.h>
+  #include "servos.h"
 #endif
 
 #ifdef USE_BASE
   #include <Servo.h>
   #include "custom_servos.h"
-  /* Motor driver function definitions */
   #include "motor_driver.h"
-
-  /* Encoder driver function definitions */
   #include "encoder_driver.h"
-
-  /* PID parameters and functions */
   #include "diff_controller.h"
 
-  /* Run the PID loop at 30 times per second */
   #define PID_RATE           30     // Hz
-
-  /* Convert the rate into an interval */
   const int PID_INTERVAL = 1000 / PID_RATE;
-  
-  /* Track the next time we make a PID calculation */
   unsigned long nextPID = PID_INTERVAL;
 
-  /* Stop the robot if it hasn't received a movement command
-   in this number of milliseconds */
   #define AUTO_STOP_INTERVAL 2000
-  long lastMotorCommand = AUTO_STOP_INTERVAL;
+  long lastMotorCommand   = AUTO_STOP_INTERVAL;
   long lastGrabberCommand = AUTO_STOP_INTERVAL;
-  long lastServoCommand = AUTO_STOP_INTERVAL;
+  long lastServoCommand   = AUTO_STOP_INTERVAL;
 #endif
 
-/* Variable initialization */
-
-// A pair of varibles to help parse serial commands (thanks Fergs)
+/* Globals for serial parsing */
 int arg = 0;
 int index = 0;
-
-// Variable to hold an input character
 char chr;
-
-// Variable to hold the current single-character command
 char cmd;
-
-// Character arrays to hold the first and second arguments
 char argv1[16];
 char argv2[16];
-
-// The arguments converted to integers
 long arg1;
 long arg2;
+
+/* Encoder/stuck‐detection specifics */
+#ifdef USE_BASE
+  // We’ll poll A0 as a digital input for the grabber encoder
+  #define ENCODER_A_PIN         A0
+
+  volatile unsigned long pulseCount         = 0;    // counts BOTH edges on A0
+  uint8_t                lastA0state        = LOW;
+
+  const unsigned long    CHECK_WINDOW       = 500UL;   // 500 ms window
+  const unsigned int     MIN_PULSES         = 30;      // ≥30 edges per window ⇒ “turning”
+  const unsigned long    REVERSE_DURATION   = 2000UL;  // 2 seconds to reverse when stuck
+
+  unsigned long          lastWindowTime       = 0;
+  unsigned long          lastWindowPulseCount = 0;
+  unsigned long          reverseUntil         = 0;     // millis until which we reverse
+  bool                   currentlyReversing   = false;
+
+  // Default grabber speed (PWM). Adjust as needed.
+  const int              DEFAULT_GRABBER_SPEED = 100;
+#endif
+
+/* Forward declarations */
+void resetCommand();
+int runCommand();
+void setGrabberSpeed(int spd);
 
 /* Clear the current command parameters */
 void resetCommand() {
@@ -160,7 +99,7 @@ void resetCommand() {
   index = 0;
 }
 
-/* Run a command.  Commands are defined in commands.h */
+/* Run a command. Commands are defined in commands.h */
 int runCommand() {
   int i = 0;
   char *p = argv1;
@@ -168,188 +107,198 @@ int runCommand() {
   int pid_args[4];
   arg1 = atoi(argv1);
   arg2 = atoi(argv2);
-  
+
   switch(cmd) {
-  case GET_BAUDRATE:
-    Serial.println(BAUDRATE);
-    break;
-  case ANALOG_READ:
-    Serial.println(analogRead(arg1));
-    break;
-  case DIGITAL_READ:
-    Serial.println(digitalRead(arg1));
-    break;
-  case ANALOG_WRITE:
-    analogWrite(arg1, arg2);
-    Serial.println("OK"); 
-    break;
-  case DIGITAL_WRITE:
-    if (arg2 == 0) digitalWrite(arg1, LOW);
-    else if (arg2 == 1) digitalWrite(arg1, HIGH);
-    Serial.println("OK"); 
-    break;
-  case PIN_MODE:
-    if (arg2 == 0) pinMode(arg1, INPUT);
-    else if (arg2 == 1) pinMode(arg1, OUTPUT);
-    Serial.println("OK");
-    break;
-  case PING:
-    Serial.println(Ping(arg1));
-    break;
+    case GET_BAUDRATE:
+      Serial.println(BAUDRATE);
+      break;
+    case ANALOG_READ:
+      Serial.println(analogRead(arg1));
+      break;
+    case DIGITAL_READ:
+      Serial.println(digitalRead(arg1));
+      break;
+    case ANALOG_WRITE:
+      analogWrite(arg1, arg2);
+      Serial.println("OK");
+      break;
+    case DIGITAL_WRITE:
+      if (arg2 == 0) digitalWrite(arg1, LOW);
+      else if (arg2 == 1) digitalWrite(arg1, HIGH);
+      Serial.println("OK");
+      break;
+    case PIN_MODE:
+      if (arg2 == 0) pinMode(arg1, INPUT);
+      else if (arg2 == 1) pinMode(arg1, OUTPUT);
+      Serial.println("OK");
+      break;
+    case PING:
+      Serial.println(Ping(arg1));
+      break;
+
 #ifdef USE_SERVOS
-  case SERVO_WRITE:
-    servos[arg1].setTargetPosition(arg2);
-    Serial.println("OK");
-    break;
-  case SERVO_READ:
-    Serial.println(servos[arg1].getServo().read());
-    break;
+    case SERVO_WRITE:
+      servos[arg1].setTargetPosition(arg2);
+      Serial.println("OK");
+      break;
+    case SERVO_READ:
+      Serial.println(servos[arg1].getServo().read());
+      break;
 #endif
-    
+
 #ifdef USE_BASE
-  case READ_ENCODERS:
-    Serial.print(readEncoder(LEFT));
-    Serial.print(" ");
-    Serial.println(readEncoder(RIGHT));
-    break;
-   case RESET_ENCODERS:
-    resetEncoders();
-    resetPID();
-    Serial.println("OK");
-    break;
-  case MOTOR_SPEEDS:
-    /* Reset the auto stop timer */
-    lastMotorCommand = millis();
-    if (arg1 == 0 && arg2 == 0) {
-      setMotorSpeeds(0, 0);
+    case READ_ENCODERS:
+      Serial.print(readEncoder(LEFT));
+      Serial.print(" ");
+      Serial.println(readEncoder(RIGHT));
+      break;
+    case RESET_ENCODERS:
+      resetEncoders();
       resetPID();
-      moving = 0;
-    }
-    else moving = 1;
-    leftPID.TargetTicksPerFrame = arg1;
-    rightPID.TargetTicksPerFrame = arg2;
-    Serial.println("OK"); 
-    break;
-  case MOTOR_RAW_PWM:
-    /* Reset the auto stop timer */
-    lastMotorCommand = millis();
-    resetPID();
-    moving = 0; // Sneaky way to temporarily disable the PID
-    setMotorSpeeds(arg1, arg2);
-    Serial.println("OK"); 
-    break;
-  case GRABBER_RAW_PWM:
-    lastGrabberCommand = millis();
-    setGrabberSpeed(arg1);
-    Serial.println("OK"); 
-    break;
-  case BATTERY_LEVEL:
-    battery_level();
-    Serial.println("OK");
-    break;
-  case ACTIVATE_SERVO:
-    lastServoCommand = millis();
-    setServo(arg1, arg2);
-    Serial.println("OK");
-    break;
-  case UPDATE_PID:
-    while ((str = strtok_r(p, ":", &p)) != '\0') {
-       pid_args[i] = atoi(str);
-       i++;
-    }
-    Kp = pid_args[0];
-    Kd = pid_args[1];
-    Ki = pid_args[2];
-    Ko = pid_args[3];
-    Serial.println("OK");
-    break;
+      Serial.println("OK");
+      break;
+    case MOTOR_SPEEDS:
+      lastMotorCommand = millis();
+      if (arg1 == 0 && arg2 == 0) {
+        setMotorSpeeds(0, 0);
+        resetPID();
+        moving = 0;
+      } else moving = 1;
+      leftPID.TargetTicksPerFrame  = arg1;
+      rightPID.TargetTicksPerFrame = arg2;
+      Serial.println("OK");
+      break;
+    case MOTOR_RAW_PWM:
+      lastMotorCommand = millis();
+      resetPID();
+      moving = 0; // temporarily disable PID
+      setMotorSpeeds(arg1, arg2);
+      Serial.println("OK");
+      break;
+    case GRABBER_RAW_PWM:
+      lastGrabberCommand = millis();
+      setGrabberSpeed(arg1);
+      Serial.println("OK");
+      break;
+    case BATTERY_LEVEL:
+      battery_level();
+      Serial.println("OK");
+      break;
+    case ACTIVATE_SERVO:
+      lastServoCommand = millis();
+      setServo(arg1, arg2);
+      Serial.println("OK");
+      break;
+    case UPDATE_PID:
+      while ((str = strtok_r(p, ":", &p)) != '\0') {
+        pid_args[i] = atoi(str);
+        i++;
+      }
+      Kp = pid_args[0];
+      Kd = pid_args[1];
+      Ki = pid_args[2];
+      Ko = pid_args[3];
+      Serial.println("OK");
+      break;
 #endif
-  default:
-    Serial.println("Invalid Command");
-    break;
+
+    default:
+      Serial.println("Invalid Command");
+      break;
   }
+  return 0;
 }
 
-/* Setup function--runs once at startup. */
+/* Helper: set grabber speed and direction. Negative = reverse.*/
+void setGrabberSpeed(int spd) {
+  unsigned char reverse = 0;
+  if (spd < 0) {
+    spd = -spd;
+    reverse = 1;
+  }
+  if (spd > MAX_PWM) {
+    spd = MAX_PWM;
+  }
+  analogWrite(GRABBER_PWM, spd);
+  digitalWrite(GRABBER_DIRECTION, reverse);
+}
+
+/* Setup function—runs once at startup. */
 void setup() {
   Serial.begin(BAUDRATE);
 
-    pinMode(LEFT_ENABLE, OUTPUT);
-    pinMode(LEFT_PWM, OUTPUT);
-    pinMode(LEFT_DIRECTION, OUTPUT);
+  pinMode(LEFT_ENABLE, OUTPUT);
+  pinMode(LEFT_PWM, OUTPUT);
+  pinMode(LEFT_DIRECTION, OUTPUT);
 
-    pinMode(LEFT_ENABLE, OUTPUT);
-    pinMode(LEFT_PWM, OUTPUT);
-    pinMode(LEFT_DIRECTION, OUTPUT);
+  pinMode(RIGHT_ENABLE, OUTPUT);
+  pinMode(RIGHT_PWM, OUTPUT);
+  pinMode(RIGHT_DIRECTION, OUTPUT);
 
-    pinMode(GRABBER_DIRECTION, OUTPUT);
-    pinMode(GRABBER_PWM, OUTPUT);
+  pinMode(GRABBER_DIRECTION, OUTPUT);
+  pinMode(GRABBER_PWM, OUTPUT);
 
-    pinMode(BATTERY_PIN, INPUT);
+  pinMode(BATTERY_PIN, INPUT);
 
-
-// Initialize the motor controller if used */
 #ifdef USE_BASE
   #ifdef ARDUINO_ENC_COUNTER
-    //set as inputs
-    DDRD &= ~(1<<LEFT_ENC_PIN_A);
-    DDRD &= ~(1<<LEFT_ENC_PIN_B);
-    DDRC &= ~(1<<RIGHT_ENC_PIN_A);
-    DDRC &= ~(1<<RIGHT_ENC_PIN_B);
-    
-    //enable pull up resistors
-    PORTD |= (1<<LEFT_ENC_PIN_A);
-    PORTD |= (1<<LEFT_ENC_PIN_B);
-    PORTC |= (1<<RIGHT_ENC_PIN_A);
-    PORTC |= (1<<RIGHT_ENC_PIN_B);
-    
-    // tell pin change mask to listen to left encoder pins
-    PCMSK2 |= (1 << LEFT_ENC_PIN_A)|(1 << LEFT_ENC_PIN_B);
-    // tell pin change mask to listen to right encoder pins
-    PCMSK1 |= (1 << RIGHT_ENC_PIN_A)|(1 << RIGHT_ENC_PIN_B);
-    
-    // enable PCINT1 and PCINT2 interrupt in the general interrupt mask
-    PCICR |= (1 << PCIE1) | (1 << PCIE2);
+    // Left encoder pins (directly on Arduino pins)
+    DDRD &= ~(1 << LEFT_ENC_PIN_A);
+    DDRD &= ~(1 << LEFT_ENC_PIN_B);
+    PORTD |= (1 << LEFT_ENC_PIN_A);
+    PORTD |= (1 << LEFT_ENC_PIN_B);
+
+    // Right encoder pins
+    DDRC &= ~(1 << RIGHT_ENC_PIN_A);
+    DDRC &= ~(1 << RIGHT_ENC_PIN_B);
+    PORTC |= (1 << RIGHT_ENC_PIN_A);
+    PORTC |= (1 << RIGHT_ENC_PIN_B);
+
+    // Enable pin‐change interrupts for left and right encoders
+    PCMSK2 |= (1 << LEFT_ENC_PIN_A) | (1 << LEFT_ENC_PIN_B);
+    PCMSK1 |= (1 << RIGHT_ENC_PIN_A) | (1 << RIGHT_ENC_PIN_B);
+    PCICR  |= (1 << PCIE1) | (1 << PCIE2);
   #endif
+
   initMotorController();
   initServos();
   resetPID();
+
+  // — Grabber‐encoder polling setup —
+  pinMode(ENCODER_A_PIN, INPUT_PULLUP);
+  lastA0state = digitalRead(ENCODER_A_PIN);
+
+  lastWindowTime       = millis();
+  lastWindowPulseCount = 0;
+  reverseUntil         = 0;
+  currentlyReversing   = false;
 #endif
 
-/* Attach servos if used */
-  #ifdef USE_SERVOS
-    int i;
-    for (i = 0; i < N_SERVOS; i++) {
-      servos[i].initServo(
-          servoPins[i],
-          stepDelay[i],
-          servoInitPosition[i]);
-    }
-  #endif
+#ifdef USE_SERVOS
+  for (int i = 0; i < N_SERVOS; i++) {
+    servos[i].initServo(
+      servoPins[i],
+      stepDelay[i],
+      servoInitPosition[i]
+    );
+  }
+#endif
 }
 
-/* Enter the main loop.  Read and parse input from the serial port
-   and run any valid commands. Run a PID calculation at the target
-   interval and check for auto-stop conditions.
-*/
+/* Main loop: read serial, run commands, PID, auto‐stop, and stuck check */
 void loop() {
+  // 1) Handle incoming serial commands
   while (Serial.available() > 0) {
-    
-    // Read the next character
     chr = Serial.read();
-
-    // Terminate a command with a CR
-    if (chr == 13) {
+    if (chr == 13) {               // CR terminates command
       if (arg == 1) argv1[index] = NULL;
       else if (arg == 2) argv2[index] = NULL;
       runCommand();
       resetCommand();
     }
-    // Use spaces to delimit parts of the command
-    else if (chr == ' ') {
-      // Step through the arguments
+    else if (chr == ' ') {         // space delimits
       if (arg == 0) arg = 1;
-      else if (arg == 1)  {
+      else if (arg == 1) {
         argv1[index] = NULL;
         arg = 2;
         index = 0;
@@ -358,50 +307,86 @@ void loop() {
     }
     else {
       if (arg == 0) {
-        // The first arg is the single-letter command
         cmd = chr;
       }
       else if (arg == 1) {
-        // Subsequent arguments can be more than one character
-        argv1[index] = chr;
-        index++;
+        argv1[index++] = chr;
       }
       else if (arg == 2) {
-        argv2[index] = chr;
-        index++;
+        argv2[index++] = chr;
       }
     }
   }
-  
-// If we are using base control, run a PID calculation at the appropriate intervals
+
 #ifdef USE_BASE
+  // 2) PID loop
   if (millis() > nextPID) {
     updatePID();
     nextPID += PID_INTERVAL;
   }
-  
-  // Check to see if we have exceeded the auto-stop interval
-  if ((millis() - lastMotorCommand) > AUTO_STOP_INTERVAL) {;
+
+  // 3) Auto‐stop for drive motors
+  if ((millis() - lastMotorCommand) > AUTO_STOP_INTERVAL) {
     setMotorSpeeds(0, 0);
     moving = 0;
   }
 
-  if ((millis() - lastGrabberCommand) > AUTO_STOP_INTERVAL) {;
+  // 4) Auto‐stop for grabber (if no grabber command recently)
+  if ((millis() - lastGrabberCommand) > AUTO_STOP_INTERVAL) {
     setGrabberSpeed(0);
   }
 
-  if ((millis() - lastServoCommand) > AUTO_STOP_INTERVAL) {;
+  // 5) Auto‐stop for servos
+  if ((millis() - lastServoCommand) > AUTO_STOP_INTERVAL) {
     setServo(0, 0);
     setServo(1, 0);
   }
+
+  // 6) Stuck‐detection: poll A0 for edges
+  unsigned long now = millis();
+  uint8_t currentA0 = digitalRead(ENCODER_A_PIN);
+  if (currentA0 != lastA0state) {
+    pulseCount++;
+    lastA0state = currentA0;
+  }
+
+  // 7) Every CHECK_WINDOW ms, evaluate pulse count
+  if (now - lastWindowTime >= CHECK_WINDOW) {
+    unsigned long pulsesInWindow = pulseCount - lastWindowPulseCount;
+
+    if (pulsesInWindow < MIN_PULSES) {
+      // Stuck: schedule 2-second reversal
+      reverseUntil = now + REVERSE_DURATION;
+      if (!currentlyReversing) {
+        currentlyReversing = true;
+      }
+    }
+    // Reset window counters
+    lastWindowPulseCount = pulseCount;
+    lastWindowTime       = now;
+  }
+
+  // 8) Drive grabber based on stuck‐state or normal operation
+  if (now < reverseUntil) {
+    // Still reversing
+    digitalWrite(GRABBER_ENABLE, LOW);  // enable driver
+    setGrabberSpeed(-DEFAULT_GRABBER_SPEED);
+  } else {
+    // Normal forward operation (or resume)
+    if (currentlyReversing) {
+      currentlyReversing = false;
+    }
+    digitalWrite(GRABBER_ENABLE, LOW);   // ensure driver is enabled
+    // If the user recently issued a GRABBER_RAW_PWM command, that speed was set in runCommand().
+    // We only override here if no recent command, so do nothing and let the last commanded speed hold.
+    // If you want always forward at DEFAULT_GRABBER_SPEED when not stuck, uncomment:
+    // setGrabberSpeed(DEFAULT_GRABBER_SPEED);
+  }
 #endif
 
-// Sweep servos
 #ifdef USE_SERVOS
-  int i;
-  for (i = 0; i < N_SERVOS; i++) {
+  for (int i = 0; i < N_SERVOS; i++) {
     servos[i].doSweep();
   }
 #endif
 }
-
