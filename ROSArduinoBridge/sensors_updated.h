e0d358a075ed55b408ce5491b85d573a5321dd28
Loïc Finette <95629530+lolothecoder@users.noreply.github.com>
2025-06-15 18:54:13 +0200

    Update sensors_updated.h

--- ROSArduinoBridge/sensors_updated.h
+++ ROSArduinoBridge/sensors_updated.h
@@ -3,7 +3,7 @@
     #define MIN_ARD_VAL 0
     #define MAX_ARD_VAL 1023
     #define MIN_BAT_VAL 0
-    #define MAX_PIN_VAL 4.2
+    #define MAX_PIN_VAL 5
     #define R7          2200
     #define R8          1100
     void battery_level();
